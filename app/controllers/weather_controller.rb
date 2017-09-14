11e581fe147029589ddbebf6fed2ea0c22d7b01b
Mark Van Akkeren <MarkVanAkkeren@gmail.com>
2017-09-14 17:23:15 -0600

    weather iteration 1 complete, can display weather on home page for selected location, needs some styling love and testing but logic good

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -2,24 +2,4 @@ class WeatherController < ApplicationController
   def index
     # @forecast = Weather.find_forecast(params[:thing])
   end
-  # def show
-  #   # @current_weather = CurrentWeather.find_current_weather(params[:destination])
-  #   # redirect_to root_path
-  # end
 end
-
-#
-# {
-#
-#     "current_observation": {
-#
-#         "display_location": {
-#             "full": "Boulder, CO"
-#         },
-#
-#         "weather": "Overcast",
-#         "temperature_string": "78.1 F (25.6 C)",
-#         "relative_humidity": "41%",
-#         "wind_string": "Calm",
-#     }
-# }
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def weather_toggle
+    if params[:destination] 
+      "layouts/yes_weather"
+    else
+      "layouts/no_weather"
+    end
+  end
+
 end
