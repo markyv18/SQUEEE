42cdf1fafd8e348b2921f0984684562490ad87da
Mark Van Akkeren <MarkVanAkkeren@gmail.com>
2017-09-14 16:14:26 -0600

    refactored controllers and presenters for home page and weather display options

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -2,10 +2,10 @@ class WeatherController < ApplicationController
   def index
     # @forecast = Weather.find_forecast(params[:thing])
   end
-  def show
-    # @current_weather = CurrentWeather.find_current_weather(params[:destination])
-    # redirect_to root_path
-  end
+  # def show
+  #   # @current_weather = CurrentWeather.find_current_weather(params[:destination])
+  #   # redirect_to root_path
+  # end
 end
 
 #
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    @cities = City.all.map { |city| city.id }
-    if params[:destination]
-      @current_weather = CurrentWeather.find_current_weather(params[:destination])
-    end
+    @presenter = WelcomePresenter.new(params)
   end
 end
