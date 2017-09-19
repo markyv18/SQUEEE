f6ff659afe0282d678bbe1b1e31ed8bb481e2f23
BFunk <bret.e.funk@gmail.com>
2017-09-19 17:47:03 -0600

    added SecureRandom.hex(15) to the Oauth password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
+require 'securerandom'
 class User < ApplicationRecord
   after_create :welcome_email
 
@@ -26,7 +27,7 @@ class User < ApplicationRecord
   end
 
   def self.find_or_create_by_auth(auth)
-    user = User.find_or_create_by(provider: auth['provider'], uid: auth['uid'], password_digest: "N/A")
+    user = User.find_or_create_by(provider: auth['provider'], uid: auth['uid'], password_digest: SecureRandom.hex(15))
     auth['info']['first_name'] ? user.first_name = auth['info']['first_name'] : user.first_name = auth['info']['name'].split(" ").first
     auth['info']['last_name'] ? user.last_name = auth['info']['last_name'] : user.last_name = auth['info']['name'].split(" ").last
     user.email = auth['info']['email']
