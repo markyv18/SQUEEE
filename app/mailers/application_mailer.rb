3d5be4670076546bc5fc09efcf8d8c924cae5efa
BFunk <bret.e.funk@gmail.com>
2017-09-19 12:07:08 -0600

    working email sent from external gmail account upon user creation

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: ENV['gmail_address']
+
   layout 'mailer'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,29 +10,28 @@ class User < ApplicationRecord
   #validates_presence_of :last_name
   #validates_presence_of :email
   #validates_presence_of :phone
-  ##validates_presence_of :password_digest
+  #validates_presence_of :password_digest
   #validates_presence_of :status
   #validates_presence_of :role
 
   #before_save :create_username
 
-  def create_username
-    self.username = "#{self.email.split("@")[0]}"
-  end
+  #def create_username
+    #self.username = "#{self.email.split("@")[0]}"
+  #end
 
   def self.sorted_descending
     self.order(:id)
   end
 
   def self.find_or_create_by_auth(auth)
-    user = User.find_or_create_by(provider: auth['provider'], uid: auth['uid'], password_digest: "test")
+    user = User.find_or_create_by(provider: auth['provider'], uid: auth['uid'], password_digest: "N/A")
     auth['info']['first_name'] ? user.first_name = auth['info']['first_name'] : user.first_name = auth['info']['name'].split(" ").first
     auth['info']['last_name'] ? user.last_name = auth['info']['last_name'] : user.last_name = auth['info']['name'].split(" ").last
     user.email = auth['info']['email']
 
     user.save
+    UserMailer.welcome_email(user).deliver
     user
   end
-
-
 end
